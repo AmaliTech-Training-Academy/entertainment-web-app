5a143f39804225a6148821b67cf094e8d2c2ce04
Wisdom-Eleeza <wisdom.eleeza@amalitech.com>
2023-01-26 16:08:29 +0000

    signup and login user interface

---
+++ Desktop/entertainmentLogin/login/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <login></login>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/entertainmentLogin/login/src/component/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const login = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          Enter your name:
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+};
