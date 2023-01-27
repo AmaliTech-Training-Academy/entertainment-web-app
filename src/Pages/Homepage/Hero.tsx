914b7cd146ec1b9d506860094ef59013d9151e33
Benedict Arhin <benedict.arhin@amalitech.com>
2023-01-27 11:23:09 +0000

    Worked on mobile media query

--- src/Pages/Homepage/Hero.tsx
+++ src/Pages/Homepage/Hero.tsx
@@ -18,7 +18,8 @@ const Hero = () => {
                                 <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z" stroke="#FFF" stroke-width="1.5" fill="none"/></svg>
                                 </div>
                                 <div className="image">
-                                    <img src={item.thumbnail.trending?.large} />
+                                    <img className="large" src={item.thumbnail.trending?.large} />
+                                    <img className="small" src={item.thumbnail.trending?.small} />
                                 </div>
                                 <div className="item-info">
                                     <div className="about">
