14db4912a72e3cc6bf7bbf3c9b52f868ac5651c6
Godbless-Abbew <godbless.abbew@amalitech.com>
2023-01-30 16:33:41 +0000

    styled movies page

--- src/Pages/Movies/Video.tsx
+++ src/Pages/Movies/Video.tsx
@@ -21,6 +21,12 @@ const Video = () => {
                                     <img className="medium" src={item.thumbnail.regular.medium} alt = ''/>
                                     <img className="small" src={item.thumbnail.regular.small} alt = ''/>
                                 </div>
+                                <div className='play-items'>
+                                    <div className="play-icon">
+                                <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg"><path d="M15 0C6.713 0 0 6.713 0 15c0 8.288 6.713 15 15 15 8.288 0 15-6.712 15-15 0-8.287-6.712-15-15-15Zm-3 21V8l9 6.5-9 6.5Z" fill="#FFF"/></svg> 
+                                <h3> Play</h3>                                    
+                                    </div>
+                                </div>
                                 <div className="item-info">
                                     <div className="about">
                                         {item.year} <div className="oval"></div> <img src={item.logo} alt = ''/> {item.category} <div className="oval"></div> {item.rating}
