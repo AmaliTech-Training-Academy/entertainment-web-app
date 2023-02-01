1b0aa7ae563a6165f74be6249b31bffe7961c2d2
ericmanu <eric.manu@amalitech.com>
2023-02-01 16:25:07 +0000

    Added a new file and new lines

--- src/Pages/Series/Videos.tsx
+++ src/Pages/Series/Videos.tsx
@@ -1,5 +1,7 @@
 import Hello from '../../data.json'
 import './Videos.css'
+import BookmarkSVG from './Bookmarked';
+
 
 
 const Videos = () => {
@@ -12,7 +14,7 @@ const Videos = () => {
                             return item.category === 'TV Series'
                         }).map((item)=> (
                             <div className="series" key={item.title}>
-                                <div className="bookmark-icon">
+                                <div className="bookmark-icon" /*onClick={BookmarkSVG}*/>
                                     <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z" stroke="#FFF" stroke-width="1.5" fill="none"/></svg>
                                 </div>
                                 <div className="image">
