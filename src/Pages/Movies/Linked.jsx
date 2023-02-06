ab77a1c16614fc51136c2981c73079b7ec50afef
Godbless-Abbew <godbless.abbew@amalitech.com>
2023-02-06 16:16:48 +0000

    styled the bookmark

--- src/Pages/Movies/Linked.jsx
+++ src/Pages/Movies/Linked.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const Link = ({ movie, handleAddBookmark, handleRemoveBookmark }) => { 
-      const [isBookmarked, setIsBookmarked] = useState(false); 
-      const handleBookmark = () => { 
-          if (isBookmarked) { 
-          handleRemoveBookmark(movie); 
-          } else { 
-          handleAddBookmark(movie); 
-          } 
-          setIsBookmarked(!isBookmarked); 
-      }; 
-      return ( 
-          <div className="movie"> 
-          <img src={movie.Poster} alt={movie.Title} /> 
-          <div className="movie-info"> 
-              <h3>{movie.Title}</h3> 
-              <span>{movie.Year}</span> 
-          </div> 
-          {/* <div className="movie-overview"> 
-              <h2>Overview:</h2> 
-              <p>{movie.Plot}</p> 
-          </div>  */}
-          <button className="bookmark-btn" onClick={handleBookmark}> 
-              {isBookmarked } 
-          </button> 
-          </div> 
-      );
+// const Link = ({ movie, handleAddBookmark, handleRemoveBookmark }) => { 
+//       const [isBookmarked, setIsBookmarked] = useState(false); 
+//       const handleBookmark = () => { 
+//           if (isBookmarked) { 
+//           handleRemoveBookmark(movie); 
+//           } else { 
+//           handleAddBookmark(movie); 
+//           } 
+//           setIsBookmarked(!isBookmarked); 
+//       }; 
+//       return ( 
+//           <div className="movie"> 
+//           <img src={movie.Poster} alt={movie.Title} /> 
+//           <div className="movie-info"> 
+//               <h3>{movie.Title}</h3> 
+//               <span>{movie.Year}</span> 
+//           </div> 
+//           {/* <div className="movie-overview"> 
+//               <h2>Overview:</h2> 
+//               <p>{movie.Plot}</p> 
+//           </div>  */}
+//           <button className="bookmark-btn" onClick={handleBookmark}> 
+//               {isBookmarked } 
+//           </button> 
+//           </div> 
+//       );
 
-      }
+//       }
 
-      export default Link;
+//       export default Link;
