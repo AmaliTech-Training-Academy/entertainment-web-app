1c83887135bb26ccb94b068bb0bee5d61549f63d
Benedict Arhin <benedict.arhin@amalitech.com>
2023-02-10 15:20:07 +0000

    Resolving conflict

---
+++ src/Components/Input.tsx
@@ -0,0 +1,12 @@
+import './Input.css'
+
+const Input = () => {
+    return (
+        <form>
+            <div className='search'></div>
+            <input type="text" placeholder="Search for TV series"/>
+        </form>
+    )
+}
+
+export default Input;
